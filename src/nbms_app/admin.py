80f549ab70f65de5ee98bf422565fd51e09ae653
TendaMudzim <t.munyai@sanbi.org.za>
2026-01-14 15:50:39 +0200

    ABAC: add governance fields

--- src/nbms_app/admin.py
+++ src/nbms_app/admin.py
@@ -6,7 +6,7 @@ from nbms_app.models import Indicator, NationalTarget, Organisation, User
 
 @admin.register(Organisation)
 class OrganisationAdmin(admin.ModelAdmin):
-    list_display = ("name", "org_type", "contact_email", "created_at")
+    list_display = ("name", "org_type", "contact_email", "is_active", "created_at")
     search_fields = ("name", "contact_email")
 
 
@@ -20,13 +20,28 @@ class CustomUserAdmin(UserAdmin):
 
 @admin.register(NationalTarget)
 class NationalTargetAdmin(admin.ModelAdmin):
-    list_display = ("code", "title", "created_at")
+    list_display = ("code", "title", "status", "sensitivity", "organisation", "created_at")
     search_fields = ("code", "title")
+    list_filter = ("status", "sensitivity", "organisation")
+
+    def save_model(self, request, obj, form, change):
+        if not obj.created_by:
+            obj.created_by = request.user
+        if not obj.organisation and getattr(request.user, "organisation", None):
+            obj.organisation = request.user.organisation
+        super().save_model(request, obj, form, change)
 
 
 @admin.register(Indicator)
 class IndicatorAdmin(admin.ModelAdmin):
-    list_display = ("code", "title", "national_target", "created_at")
+    list_display = ("code", "title", "national_target", "status", "sensitivity", "organisation", "created_at")
     search_fields = ("code", "title")
-    list_filter = ("national_target",)
+    list_filter = ("national_target", "status", "sensitivity", "organisation")
+
+    def save_model(self, request, obj, form, change):
+        if not obj.created_by:
+            obj.created_by = request.user
+        if not obj.organisation and getattr(request.user, "organisation", None):
+            obj.organisation = request.user.organisation
+        super().save_model(request, obj, form, change)
 
