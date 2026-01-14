b3b3d2e8f0956f95ea4addf540d1a89b14872964
TendaMudzim <t.munyai@sanbi.org.za>
2026-01-14 15:29:16 +0200

    Manage: users UI

--- src/nbms_app/urls.py
+++ src/nbms_app/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
     path("manage/organisations/", views.manage_organisation_list, name="manage_organisation_list"),
     path("manage/organisations/new/", views.manage_organisation_create, name="manage_organisation_create"),
     path("manage/organisations/<int:org_id>/edit/", views.manage_organisation_edit, name="manage_organisation_edit"),
+    path("manage/users/", views.manage_user_list, name="manage_user_list"),
+    path("manage/users/new/", views.manage_user_create, name="manage_user_create"),
+    path("manage/users/<int:user_id>/edit/", views.manage_user_edit, name="manage_user_edit"),
+    path("manage/users/<int:user_id>/send-reset/", views.manage_user_send_reset, name="manage_user_send_reset"),
 ]
