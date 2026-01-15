510f627c0aa3c18167d296d15d3e2909491f4863
TendaMudzim <t.munyai@sanbi.org.za>
2026-01-15 08:58:31 +0200

    Add seed_report_templates command

---
+++ src/nbms_app/tests/test_report_sections_seed.py
@@ -0,0 +1,13 @@
+from django.core.management import call_command
+from django.test import TestCase
+
+from nbms_app.models import ReportSectionTemplate
+
+
+class ReportSectionSeedTests(TestCase):
+    def test_seed_report_templates(self):
+        self.assertEqual(ReportSectionTemplate.objects.count(), 0)
+        call_command("seed_report_templates")
+        self.assertEqual(ReportSectionTemplate.objects.count(), 5)
+        call_command("seed_report_templates")
+        self.assertEqual(ReportSectionTemplate.objects.count(), 5)
